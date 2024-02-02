eb0226962aa9e0ddbb72750efd2b1fad4d50cba5
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2024-02-02 11:35:14 +0100

    add pl package functions

---
+++ src/FlowSOM/pl/__init__.py
@@ -0,0 +1,13 @@
+from ._plot_helper_functions import (
+    FlowSOM_colors,
+    add_legend,
+    add_nodes,
+    add_stars,
+    add_text,
+    add_wedges,
+    gg_color_hue,
+    plot_FlowSOM,
+    plot_star_legend,
+    scale_star_heights,
+)
+from .plot_functions import FlowSOMmary, plot_2D_scatters, plot_marker, plot_pies, plot_stars, plot_variable
