9d9bf564718b89c86143473f9034c0e400d626cc
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2024-03-26 22:50:58 +0100

    fix score

--- tests/models/test_SOMModel.py
+++ tests/models/test_SOMModel.py
@@ -14,4 +14,4 @@ def test_clustering_v_measure(X_and_y):
     X, y_true = X_and_y
     y_pred = som.fit_predict(X)
     score = v_measure_score(y_true, y_pred)
-    assert score == 0.7
+    assert score > 0.7
