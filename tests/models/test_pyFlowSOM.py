26b0d85e7d136fedb4632a127bb54416e298fa92
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2025-04-28 10:44:34 +0200

    skip tests if pyflowsom not installed

--- tests/models/test_pyFlowSOM.py
+++ tests/models/test_pyFlowSOM.py
@@ -1,21 +1,21 @@
 import pytest
 from sklearn.metrics import v_measure_score
 
-# optional import if pyFlowSOM is installed, otherwise use regular FlowSOM for type checking
+# Skip the entire test file if pyflowsom is not installed
+pytest.importorskip("pyFlowSOM", reason="pyflowsom is not installed")
+
 try:
     from flowsom.models.pyflowsom_estimator import PyFlowSOMEstimator
 except ImportError:
     from flowsom.models import FlowSOMEstimator as PyFlowSOMEstimator
 
 
-@pytest.importorskip("flowsom.models.pyflowsom_estimator")
 def test_clustering(X):
     fsom = PyFlowSOMEstimator(n_clusters=10)
     y_pred = fsom.fit_predict(X)
     assert y_pred.shape == (100,)
 
 
-@pytest.importorskip("flowsom.models.pyflowsom_estimator")
 def test_clustering_v_measure(X_and_y):
     som = PyFlowSOMEstimator(n_clusters=10)
     X, y_true = X_and_y
@@ -24,7 +24,6 @@ def test_clustering_v_measure(X_and_y):
     assert score > 0.7
 
 
-@pytest.importorskip("flowsom.models.pyflowsom_estimator")
 def test_reproducibility_no_seed(X):
     fsom_1 = PyFlowSOMEstimator(n_clusters=10)
     fsom_2 = PyFlowSOMEstimator(n_clusters=10)
@@ -34,7 +33,6 @@ def test_reproducibility_no_seed(X):
     assert not all(y_pred_1 == y_pred_2)
 
 
-@pytest.importorskip("flowsom.models.pyflowsom_estimator")
 def test_reproducibility_seed(X):
     fsom_1 = PyFlowSOMEstimator(n_clusters=10, seed=0)
     fsom_2 = PyFlowSOMEstimator(n_clusters=10, seed=0)
